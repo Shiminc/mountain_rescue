e699091b38951c8a56f5c5f35ad5636ab3a4689e
Shiminc <shiminc@gmail.com>
2026-08-23 19:55:24 +0100

    utils

--- scripts/utils/plot.py
+++ scripts/utils/plot.py
@@ -6,11 +6,27 @@ import numpy as np
 import altair as alt
 import os 
 
-def set_up_altair():
+def set_up_altair_browser():
     alt.renderers.enable('browser')
     alt.renderers.set_embed_options(loader={"target": "_blank"})
     #alt.renderers.enable('mimetype') # offline renderer
     alt.data_transformers.disable_max_rows()
+
+def set_up_altair_jupyter():
+    alt.renderers.enable('mimetype') # offline renderer
+
+
+def create_year_month_line_chart(df):
+    base = alt.Chart(df).encode(
+    x = alt.X('yearmonth(date):T')).configure_view(
+    continuousWidth=1200,
+    )
+
+    line = base.mark_line().encode(
+    y = alt.Y('count():Q'),
+    tooltip=alt.Tooltip(['yearmonth(date):T','count():Q'])
+    )
+    return line
 
 def create_histogram(df, var_x, bin=True):
     bar_chart = alt.Chart(df).mark_bar().encode(
@@ -26,5 +42,26 @@ def create_stacked_bar(df, var_x, stacked_var):
         y = string_y,
         color = stacked_var
     )
+
+    return bar_chart
+
+def create_stacked_bar_by_date(df,time='year'):
+    chart_height = 300
+
+    if time == 'year':
+        x_var = 'year(date)'
+        chart_width = 600
+    elif time == 'yearmonth':
+        x_var = 'yearmonth(date)'
+        chart_width = 1000
+
+    bar_chart = alt.Chart(df).mark_bar().encode(
+        x = x_var ,
+        y = 'count(Incident_Cause)',
+        color = 'Incident_Cause:N'
+    ).properties(
+        width = chart_width,
+        height = chart_height
+        )
 
     return bar_chart
--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -1,10 +1,11 @@
+"""
+This module contains functions used for preprocessing data.
+"""
 import pandas as pd
 import json
-import time
-from datetime import timedelta, date, datetime 
+from datetime import datetime 
 import numpy as np
-import altair as alt
-import os 
+
 
 PATH = "../../data/all_incidents.json"
 HOLIDAY_PATH = "../../data/ukbankholidays-jul19.csv"
@@ -126,14 +127,13 @@ def aggregate_by_year_month(df, start_date='2015-01-01', end_date='2025-12-31', 
 
 def handling_problematic_data(data):
     # empty cell in incident cause to be recoded to another original value Other
-    # data.Incident_Cause[data.Incident_Cause=='']='Other'
-    # data['Incident_Cause'].replace({'': 'Other'})
     # other could include help sweeping snow or flood or dog https://www.wmrt.org.uk/incidents/corney-fell-sat-11th-mar-2023/, https://www.wmrt.org.uk/incidents/brown-tongue-scafell-pike-thu-1st-jan-1970/
-    
     data.loc[data['Incident_Cause']=='','Incident_Cause']='Other' 
-    # based on the mean number of staff of the full callout and limited callout - 15 vs 7, current data only 6 count of callout which is quite seprate, 2, 4, then 11 , so use 10 as cut off
+
+    # 6 Callout - based on the mean number of staff of the full callout and limited callout - 15 vs 7, current data only 6 count of callout which is quite seperate, 2, 4, then 11 , so use 10 as cut off
     data.loc[(data['Incident_Type']=='Callout') & (data['staff'] >=10),'Incident_Type']='Full Callout'
     data.loc[(data['Incident_Type']=='Callout') & (data['staff'] <10),'Incident_Type']='Limited Callout'
+
     # missing data on Incident_Type, I read through and assign based on my judgement
     data.loc[data["Incident"]=='106 in 2025', 'Incident_Type']='Alert'
     data.loc[data["Incident"]=='38 in 2025', 'Incident_Type']='Full Callout'
@@ -141,6 +141,7 @@ def handling_problematic_data(data):
     data.loc[data["Incident"]=='133 in 2023', 'Incident_Type']='Alert'
     data.loc[data["Incident"]=='95 in 2023', 'Incident_Type']='Full Callout'
     data.loc[data["Incident"]=='55 in 2023', 'Incident_Type']='Limited Callout'
+
     # odd number of staff for small alerts due to the big number of staff around for other incidents or training, change the number to reflect based on reading of incident reports
     # to avoid inflation of total_hrs 
     data.loc[data["Incident"]=='117 in 2025', 'staff'] = 1
@@ -149,9 +150,12 @@ def handling_problematic_data(data):
     data.loc[data["Incident"]=='2 in 2023', 'total_hrs'] = 0.6
     data.loc[data["Incident"]=='121 in 2021', 'staff'] = 1
     data.loc[data["Incident"]=='121 in 2021', 'total_hrs'] = 4.3
-    # replace with mean number of staff as again training nearby with 22 members
+
+
+    # replace with mean number of staff as again training nearby with 22 members, as no clear mentions of staff
     data.loc[data["Incident"]=='57 in 2017', 'staff'] = 7
     data.loc[data["Incident"]=='57 in 2017', 'total_hrs'] = 20.3
+
     # drop rows with hrs or staff as NaN, most are either short alert or flood responding rather than mountain rescue
     data = data.dropna(subset=['hrs','staff','date'])
 
@@ -187,6 +191,7 @@ def determine_next_day(data):
 
     return data
 
+# main functions to be used in other main scripts. 
 def preprocess_data():
     data = read_json_to_df(PATH)
     data = format_time_columns(data)
@@ -197,3 +202,4 @@ def preprocess_data():
     data = determine_bankholiday(data)
     data = data[(data['year']>2014) & (data['year']<2026)]
     return data
+
